bdc974f914d327e86926793a83fcfae2841aa9c0
Hiram Silvey <hiram@Hirams-MacBook-Pro.local>
2019-05-12 15:15:28 -0400

    fixed misc formatting

--- init.el
+++ init.el
@@ -1,8 +1,6 @@
 ;;; .init.el --- Load the full configuration -*- lexical-binding: t -*-
-;;; Commentary:
 
-;; This file bootstraps the configuration, which is divided into
-;; a number of other files.
+;;; Commentary:
 
 ;;; Code:
 
