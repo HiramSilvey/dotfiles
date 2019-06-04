032e71502bdab4da1bb246467939ed8fa994b585
Hiram Silvey <blujay270@gmail.com>
2019-06-04 13:38:32 -0400

    updated org file with custom states

--- org.el
+++ org.el
@@ -19,4 +19,15 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 
+;; Add custom TODO states
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "CURRENT(c)" "|" "DONE(d)" "CANCELED(x@)")))
+
+;; Add custom face colors for custom TODO states
+(setq org-todo-keyword-faces
+      '(("CURRENT" . (:foreground "light yellow" :inverse-video t :weight bold)) ("CANCELED" . (:foreground "light grey" :inverse-video t :weight bold))))
+
+;; Log the time when the TODO state of an item changes to a DONE state
+(setq org-log-done 'time)
+
 ;;; org.el ends here
