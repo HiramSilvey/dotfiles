2f83a65ff20de904aa41b7281be7ae3b79cbe920
Hiram Silvey <blujay270@gmail.com>
2019-07-10 16:53:16 -0400

    fixed keybinding swap and added newline breaks for markdown file editing

--- keybindings.el
+++ keybindings.el
@@ -6,9 +6,12 @@
 ;;; Code:
 
 ;; Swap the u and x keys for all emacs commands
-(define-key key-translation-map [?\C-x] [?\C-u])
-(define-key key-translation-map [?\C-u] [?\C-x])
-(define-key key-translation-map [?\M-x] [?\M-u])
+(defun swap-ctl-u-ctl-x (frame)
+    "Swap the ctl-u and ctl-x keys in the newly created FRAME."
+    (with-selected-frame frame
+      (define-key input-decode-map [?\C-x] [?\C-u])
+      (define-key input-decode-map [?\C-u] [?\C-x])))
+(add-hook 'after-make-frame-functions #'swap-ctl-u-ctl-x)
 (define-key key-translation-map [?\M-u] [?\M-x])
 
 ;;; keybindings.el ends here
