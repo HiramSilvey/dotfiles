d2bbc0b77224616bac42c155f4dc8609a278bba9
Hiram Silvey <hiramj@google.com>
2019-12-10 15:21:57 +0000

    removed PENDING org state and added support for auto changing state to DONE when children tasks are DONE

--- org.el
+++ org.el
@@ -25,12 +25,19 @@
 
 ;; Add custom TODO states and prompt for a note when canceled
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "PENDING(p)" "|" "DONE(d)" "CANCELED(x@)")))
+      '((sequence "TODO(t)" "|" "DONE(d)" "CANCELED(x@)")))
+
+;; Change TODO entries to DONE when all children are done
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
 
 ;; Add custom face colors for custom TODO states
 (setq org-todo-keyword-faces
-      '(("PENDING" . (:foreground "DarkGoldenRod" :background "LightGoldenRod" :weight bold))
-        ("CANCELED" . (:foreground "light grey" :inverse-video t :weight bold))))
+      '(("CANCELED" . (:foreground "light grey" :inverse-video t :weight bold))))
 
 ;; Log the date when the TODO state of an item changes to a DONE state
 (setq org-log-done-with-time nil) ;; Do not log the time with the date
