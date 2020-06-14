bab79808f6e1e69570544df7850c0bcce14ded1d
Hiram Silvey <blujay270@gmail.com>
2020-06-14 02:36:22 -0400

    Added basic fish shell configuration

---
+++ config.fish
@@ -0,0 +1,17 @@
+# Support fish shell in emacs.
+if test -n "$EMACS"
+    set -x TERM eterm-color
+end
+function fish_title
+    true
+end
+
+# Set aliases.
+alias e="emacsclient -c -a=''"
+alias te="emacsclient -t -a=''"
+alias ke="emacsclient -e '(save-buffers-kill-emacs)'"
+
+# Load additional local configurations.
+if test -e $HOME/.config/fish/local.fish
+    source $HOME/.config/fish/local.fish
+end
