ce2772bc64ea4cce46844466de61915c5116ef06
Hiram-Silvey <blujay270@gmail.com>
2020-06-29 11:58:42 -0400

    Minor adjustments to tmux and fish configs

--- config.fish
+++ config.fish
@@ -11,6 +11,10 @@ alias e="emacsclient -c -a=''"
 alias te="emacsclient -t -a=''"
 alias ke="emacsclient -e '(save-buffers-kill-emacs)'"
 
+# Set env variables.
+set -x EDITOR emacs
+set -x ALTERNATE_EDITOR emacs
+
 # Load additional local configurations.
 if test -e $HOME/.config/fish/local.fish
     source $HOME/.config/fish/local.fish
