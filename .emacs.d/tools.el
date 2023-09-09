9f9acadba1a2767889d8a1951f367d2d74ad682c
Hiram Silvey <blujay270@gmail.com>
2023-09-09 16:03:34 -0400

    Removed LSP mode in favor of emacs 30's built-in eglot mode. Breadcrumbs soon to follow.

--- .emacs.d/tools.el
+++ .emacs.d/tools.el
@@ -11,30 +11,6 @@
   :after flycheck
   :hook (flycheck-mode . flycheck-popup-tip-mode))
 
-;; Featureful LSP support.
-(use-package lsp-mode
-  :ensure t
-  :commands lsp
-  :init (setq lsp-keymap-prefix "C-c l")
-  :hook
-  (lsp-mode . lsp-enable-which-key-integration)
-  ((c++-mode c++-ts-mode rust-mode rust-ts-mode) . lsp)
-  :config (setq lsp-headerline-breadcrumb-segments '(symbols)))
-
-;; `lsp-mode' supported debugger.
-(use-package dap-mode
-  :ensure t
-  :after lsp-mode
-  :functions (dap-register-debug-template)
-  ;; Support debugging Rust.
-  :config (dap-register-debug-template "Rust::GDB Run Configuration"
-                                       (list :type "gdb"
-                                             :request "launch"
-                                             :name "GDB::Run"
-                                             :gdbpath "rust-gdb"
-                                             :target nil
-                                             :cwd nil)))
-
 ;; Git support.
 (use-package magit
   :ensure t)
