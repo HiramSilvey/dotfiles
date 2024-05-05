f13ed153bc21f2cb9c463c29561e6e614ed4919a
Hiram Silvey <blujay270@gmail.com>
2024-05-05 00:23:35 -0400

    Move vertico configurations into relevant use-package declaration.

--- .emacs.d/search.el
+++ .emacs.d/search.el
@@ -5,9 +5,34 @@
 ;; Dired using fd.
 (use-package fd-dired)
 
+;; Vertico dependency -- see use below.
+(use-package crm)
+
 ;; VERTical Interactive COmpletion.
 (use-package vertico
-  :init (vertico-mode))
+  :init (vertico-mode)
+  :hook
+  (minibuffer-setup . cursor-intangible-mode)
+  :custom
+  (minibuffer-prompt-properties
+   '(read-only t cursor-intangible t face minibuffer-prompt)
+   "Do not allow the cursor in the minibuffer prompt.")
+  (read-extended-command-predicate
+   'command-completion-default-include-p
+   (concat "Emacs 28: Hide commands in M-x which do not work in the current mode. "
+           "Vertico commands are hidden in normal buffers."))
+  (enable-recursive-minibuffers t)
+  :config
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add 'completing-read-multiple :filter-args 'crm-indicator))
 
 ;; Optionally use the `orderless' completion style. See
 ;; `+orderless-dispatch' in the Consult wiki for an advanced Orderless style
@@ -34,35 +59,5 @@
   ;; Bind `marginalia-cycle' only in the minibuffer
   :bind (:map minibuffer-local-map
               ("M-A" . marginalia-cycle)))
-
-;; Vertico dependency -- see use below.
-(use-package crm)
-
-;; Additional useful vertico configurations.
-(use-package emacs
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-  (defun crm-indicator (args)
-    (cons (format "[CRM%s] %s"
-                  (replace-regexp-in-string
-                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                   crm-separator)
-                  (car args))
-          (cdr args)))
-  (advice-add 'completing-read-multiple :filter-args 'crm-indicator)
-
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook 'cursor-intangible-mode)
-
-  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
-  ;; Vertico commands are hidden in normal buffers.
-  (setq read-extended-command-predicate
-        'command-completion-default-include-p)
-
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t))
 
 ;;; search.el ends here
