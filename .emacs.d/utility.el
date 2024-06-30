cb14feaca88a9696418c635c18d152b5c3f31639
Hiram Silvey <blujay270@gmail.com>
2024-06-30 13:29:59 -0400

    Integrate embark.

--- .emacs.d/utility.el
+++ .emacs.d/utility.el
@@ -33,6 +33,40 @@
                    (setq-local corfu-auto nil)
                    (corfu-mode))))
 
+;; Emacs Mini-Buffer Actions Rooted in Keymaps. I.e. a super-charged
+;; right-click.
+(use-package embark
+  :bind
+  (("C-." . embark-act)
+   ("C-;" . embark-dwim)
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers.
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Integrate embark with consult.
+(use-package embark-consult
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
+
 ;; Ensure exec-path matches the shell $PATH.
 (use-package exec-path-from-shell
   :custom (exec-path-from-shell-arguments nil)
