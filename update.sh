afe023999bc7ad151e245fce5c5d0460daf00932
Hiram Silvey <blujay270@gmail.com>
2024-08-16 22:12:32 -0400

    Add update script.

---
+++ update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Exit the script if any errors are encountered.
+set -e
+
+# Print commands to stdout as they are run.
+set -o xtrace
+
+# go
+go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/cmd/goimports@latest
+
+# emacs
+mkdir -p $HOME/src/public/apps
+[ -d $HOME/src/public/apps/emacs ] || git clone git://git.sv.gnu.org/emacs.git $HOME/src/public/apps/emacs
+cd $HOME/src/public/apps/emacs
+git clean -dfx
+git pull
+./autogen.sh
+./configure --without-compress-install --with-native-compilation --with-mailutils --with-pgtk --with-tree-sitter
+make -j $(nproc)
+sudo make install
+
+# fonts
+emacs --eval "(progn (all-the-icons-install-fonts t) (nerd-icons-install-fonts t) (kill-emacs))"
+fc-cache -fv
+
+# clipse
+[ -d $HOME/src/public/apps/clipse ] || git clone https://github.com/savedra1/clipse.git $HOME/src/public/apps/clipse
+cd $HOME/src/public/apps/clipse
+git pull
+go mod tidy
+go build -o clipse
+sudo mv clipse /usr/local/bin/clipse
+
+# hyprland contrib scratchpad
+[ -d $HOME/src/public/contrib ] || git clone https://github.com/hyprwm/contrib.git $HOME/src/public/contrib
+cd $HOME/src/public/contrib
+git pull
+cd scratchpad
+sed -i s/"_menu_cmd=.*"/"_menu_cmd=\"fuzzel --dmenu -p scratchpad\""/g scratchpad
+sudo make install
