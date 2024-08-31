163039424d02bb6e98c15fc9f811d3fb57dd3139
Hiram Silvey <blujay270@gmail.com>
2024-08-31 00:14:13 -0400

    Update install scripts and backup ags config.

--- install.sh
+++ install.sh
@@ -10,8 +10,9 @@ SCRIPT_DIRNAME=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pw
 SCRIPT_BASENAME=$(basename $SCRIPT_DIRNAME)
 
 sudo dnf copr enable solopasha/hyprland
+sudo dnf copr enable azandure/clipse
 
-sudo dnf install -y hyprland hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland qt5-qtwayland qt6-qtwayland pipewire wireplumber waybar dolphin firefox pavucontrol socat zsh stow curl git go cmake libtool libvterm grim slurp fuzzel qt6ct kvantum plasma-breeze-qt6 lz4-devel btop bluez hyprpicker NetworkManager wl-clipboard brightnessctl aylurs-gtk-shell
+sudo dnf install -y hyprland hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland qt5-qtwayland qt6-qtwayland pipewire wireplumber waybar dolphin firefox pavucontrol socat zsh stow curl git go cmake libtool libvterm grim slurp fuzzel qt6ct kvantum plasma-breeze-qt6 lz4-devel btop bluez hyprpicker NetworkManager wl-clipboard brightnessctl aylurs-gtk-shell clipse
 
 # rust
 which rustup || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -27,29 +28,6 @@ rustup update
 # go
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
-
-# emacs
-mkdir -p $HOME/src/public/apps
-[ -d $HOME/src/public/apps/emacs ] || git clone git://git.sv.gnu.org/emacs.git $HOME/src/public/apps/emacs
-cd $HOME/src/public/apps/emacs
-git clean -dfx
-git pull
-./autogen.sh
-./configure --without-compress-install --with-native-compilation --with-mailutils --with-pgtk --with-tree-sitter
-make -j $(nproc)
-sudo make install
-
-# fonts
-emacs --eval "(progn (all-the-icons-install-fonts t) (nerd-icons-install-fonts t) (kill-emacs))"
-fc-cache -fv
-
-# clipse
-[ -d $HOME/src/public/apps/clipse ] || git clone https://github.com/savedra1/clipse.git $HOME/src/public/apps/clipse
-cd $HOME/src/public/apps/clipse
-git pull
-go mod tidy
-go build -o clipse
-sudo mv clipse /usr/local/bin/clipse
 
 # hyprland contrib scratchpad
 [ -d $HOME/src/public/contrib ] || git clone https://github.com/hyprwm/contrib.git $HOME/src/public/contrib
@@ -75,12 +53,25 @@ curl -fsSL https://bun.sh/install | bash && \
 [ -d $HOME/src/public/HyprPanel ] || git clone https://github.com/Jas-SinghFSU/HyprPanel.git $HOME/src/public/HyprPanel
 cd $HOME/src/public/HyprPanel
 git pull
-[ -d $HOME/.config/ags ] || mv $HOME/.config/ags $HOME/.config/ags.bkup
-ln -s $(pwd)/HyprPanel $HOME/.config/ags
+[ ! -d $HOME/.config/ags ] || mv -f $HOME/.config/ags $HOME/.config/ags.bkup
+ln -s $HOME/src/public/HyprPanel $HOME/.config/ags
+
+# emacs
+mkdir -p $HOME/src/public/apps
+[ -d $HOME/src/public/apps/emacs ] || git clone git://git.sv.gnu.org/emacs.git $HOME/src/public/apps/emacs
+cd $HOME/src/public/apps/emacs
+git clean -dfx
+git pull
+./autogen.sh
+./configure --without-compress-install --with-native-compilation --with-mailutils --with-pgtk --with-tree-sitter
+make -j $(nproc)
+sudo make install
+emacs --eval "(progn (all-the-icons-install-fonts t) (nerd-icons-install-fonts t) (kill-emacs))"
+fc-cache -fv
 
 # Finish install.
-[ -d $HOME/.emacs.d ] && mv $HOME/.emacs.d $HOME/.emacs.d.old
-[ -d $HOME/.zshrc ] && mv $HOME/.zshrc $HOME/.zshrc.old
+[ ! -d $HOME/.emacs.d ] || mv -f $HOME/.emacs.d $HOME/.emacs.d.old
+[ ! -d $HOME/.zshrc ] || mv -f $HOME/.zshrc $HOME/.zshrc.old
 
 cd ${SCRIPT_DIRNAME}/..
 stow $SCRIPT_BASENAME -t ${HOME}/
--- update.sh
+++ update.sh
@@ -6,35 +6,18 @@ set -e
 # Print commands to stdout as they are run.
 set -o xtrace
 
+# dnf
+sudo dnf update
+
+# flatpak
+! which flatpak || flatpak update
+
 # rust
 rustup update
 
 # go
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
-
-# emacs
-mkdir -p $HOME/src/public/apps
-[ -d $HOME/src/public/apps/emacs ] || git clone git://git.sv.gnu.org/emacs.git $HOME/src/public/apps/emacs
-cd $HOME/src/public/apps/emacs
-git clean -dfx
-git pull
-./autogen.sh
-./configure --without-compress-install --with-native-compilation --with-mailutils --with-pgtk --with-tree-sitter
-make -j $(nproc)
-sudo make install
-
-# fonts
-emacs --eval "(progn (all-the-icons-install-fonts t) (nerd-icons-install-fonts t) (kill-emacs))"
-fc-cache -fv
-
-# clipse
-[ -d $HOME/src/public/apps/clipse ] || git clone https://github.com/savedra1/clipse.git $HOME/src/public/apps/clipse
-cd $HOME/src/public/apps/clipse
-git pull
-go mod tidy
-go build -o clipse
-sudo mv clipse /usr/local/bin/clipse
 
 # swww
 [ -d $HOME/src/public/swww ] || git clone https://github.com/LGFae/swww.git $HOME/src/public/swww
@@ -46,14 +29,14 @@ sudo mv target/release/swww-daemon /usr/local/bin/swww-daemon
 
 # bun
 curl -fsSL https://bun.sh/install | bash && \
-  sudo ln -s $HOME/.bun/bin/bun /usr/local/bin/bun
+  sudo ln -sf $HOME/.bun/bin/bun /usr/local/bin/bun
 
 # HyprPanel
 [ -d $HOME/src/public/HyprPanel ] || git clone https://github.com/Jas-SinghFSU/HyprPanel.git $HOME/src/public/HyprPanel
 cd $HOME/src/public/HyprPanel
 git pull
-[ -d $HOME/.config/ags ] || mv $HOME/.config/ags $HOME/.config/ags.bkup
-ln -s $(pwd)/HyprPanel $HOME/.config/ags
+[ ! -d $HOME/.config/ags ] || mv -f $HOME/.config/ags $HOME/.config/ags.bkup
+ln -sf $HOME/src/public/HyprPanel $HOME/.config/ags
 
 # hyprland contrib scratchpad
 [ -d $HOME/src/public/contrib ] || git clone https://github.com/hyprwm/contrib.git $HOME/src/public/contrib
@@ -63,7 +46,20 @@ cd scratchpad
 sed -i s/"_menu_cmd=.*"/"_menu_cmd=\"fuzzel --dmenu -p scratchpad\""/g scratchpad
 sudo make install
 
+# emacs
+mkdir -p $HOME/src/public/apps
+[ -d $HOME/src/public/apps/emacs ] || git clone git://git.sv.gnu.org/emacs.git $HOME/src/public/apps/emacs
+cd $HOME/src/public/apps/emacs
+git clean -dfx
+git pull
+./autogen.sh
+./configure --without-compress-install --with-native-compilation --with-mailutils --with-pgtk --with-tree-sitter
+make -j $(nproc)
+sudo make install
+emacs --eval "(progn (all-the-icons-install-fonts t) (nerd-icons-install-fonts t) (kill-emacs))"
+fc-cache -fv
+
 echo "To update dart-sass:
 1. Download and extract the latest release from https://github.com/sass/dart-sass/releases/
 2. sudo mv dart-sass /opt/
-3. sudo ln -s /opt/dart-sass/sass /usr/local/bin/sass"
+3. sudo ln -sf /opt/dart-sass/sass /usr/local/bin/sass"
