1beda9c000ca990dc3051abeec68fa74a441b2a9
Hiram Silvey <blujay270@gmail.com>
2024-09-15 15:29:37 -0400

    Update script to not recompile if changes didn't occur.

--- update.sh
+++ update.sh
@@ -22,10 +22,13 @@ go install golang.org/x/tools/cmd/goimports@latest
 # swww
 [ -d $HOME/src/public/swww ] || git clone https://github.com/LGFae/swww.git $HOME/src/public/swww
 cd $HOME/src/public/swww
-git pull
-cargo build --release
-sudo mv target/release/swww /usr/local/bin/swww
-sudo mv target/release/swww-daemon /usr/local/bin/swww-daemon
+git fetch
+if [ $(git rev-parse @) != $(git rev-parse @{u}) ]; then
+    git pull
+    cargo build --release
+    sudo mv target/release/swww /usr/local/bin/swww
+    sudo mv target/release/swww-daemon /usr/local/bin/swww-daemon
+fi
 
 # bun
 curl -fsSL https://bun.sh/install | bash && \
@@ -34,37 +37,49 @@ curl -fsSL https://bun.sh/install | bash && \
 # HyprPanel
 [ -d $HOME/src/public/HyprPanel ] || git clone https://github.com/Jas-SinghFSU/HyprPanel.git $HOME/src/public/HyprPanel
 cd $HOME/src/public/HyprPanel
-git pull
-[ ! -d $HOME/.config/ags ] || mv -f $HOME/.config/ags $HOME/.config/ags.bkup
-ln -sf $HOME/src/public/HyprPanel $HOME/.config/ags
+git fetch
+if [ $(git rev-parse @) != $(git rev-parse @{u}) ]; then
+    git pull
+    [ ! -d $HOME/.config/ags ] || mv -f $HOME/.config/ags $HOME/.config/ags.bkup
+    ln -sf $HOME/src/public/HyprPanel $HOME/.config/ags
+fi
 
 # hyprland contrib scratchpad
 [ -d $HOME/src/public/contrib ] || git clone https://github.com/hyprwm/contrib.git $HOME/src/public/contrib
 cd $HOME/src/public/contrib
-git pull
-cd scratchpad
-sed -i s/"_menu_cmd=.*"/"_menu_cmd=\"fuzzel --dmenu -p scratchpad\""/g scratchpad
-sudo make install
+git fetch
+if [ $(git rev-parse @) != $(git rev-parse @{u}) ]; then
+    git pull
+    cd scratchpad
+    sed -i s/"_menu_cmd=.*"/"_menu_cmd=\"fuzzel --dmenu -p scratchpad\""/g scratchpad
+    sudo make install
+fi
 
 # nwg-look
 [ -d $HOME/src/public/nwg-look ] || git clone https://github.com/nwg-piotr/nwg-look.git $HOME/src/public/nwg-look
 cd $HOME/src/public/nwg-look
-git pull
-make build
-sudo make install
+git fetch
+if [ $(git rev-parse @) != $(git rev-parse @{u}) ]; then
+    git pull
+    make build
+    sudo make install
+fi
 
 # emacs
 mkdir -p $HOME/src/public/apps
 [ -d $HOME/src/public/apps/emacs ] || git clone git://git.sv.gnu.org/emacs.git $HOME/src/public/apps/emacs
 cd $HOME/src/public/apps/emacs
 git clean -dfx
-git pull
-./autogen.sh
-./configure --without-compress-install --with-native-compilation --with-mailutils --with-pgtk --with-tree-sitter
-make -j $(nproc)
-sudo make install
-emacs --eval "(progn (straight-pull-all) (straight-check-all) (kill-emacs))"
-fc-cache -fv
+git fetch
+if [ $(git rev-parse @) != $(git rev-parse @{u}) ]; then
+    git pull
+    ./autogen.sh
+    ./configure --without-compress-install --with-native-compilation --with-mailutils --with-pgtk --with-tree-sitter
+    make -j $(nproc)
+    sudo make install
+    emacs --eval "(progn (straight-pull-all) (straight-check-all) (kill-emacs))"
+    fc-cache -fv
+fi
 
 echo "To update dart-sass:
 1. Download and extract the latest release from https://github.com/sass/dart-sass/releases/
