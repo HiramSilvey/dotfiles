e444e285a0f0f856bd2dac87c6485ed3099b957e
Hiram Silvey <9485196+HiramSilvey@users.noreply.github.com>
2024-11-09 20:52:22 -0500

    Add GitHub Copilot package.

--- .emacs.d/utility.el
+++ .emacs.d/utility.el
@@ -15,6 +15,22 @@
           json-ts-mode)
          . clang-format-on-save))
 
+;; GitHub Copilot.
+(use-package copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-p" . 'copilot-previous-completion)
+              ("C-n" . 'copilot-next-completion))
+  :config
+  (add-to-list 'copilot-indentation-alist '(prog-mode . 2))
+  (add-to-list 'copilot-indentation-alist '(org-mode . 2))
+  (add-to-list 'copilot-indentation-alist '(text-mode . 2))
+  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode . 2)))
+
 ;; Completion in region function.
 (use-package corfu
   ; Free the RET key for less intrusive behavior.
