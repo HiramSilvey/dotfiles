58d6193218487c5f650fd9b2bf9ddceaff56f1be
Hiram Silvey <9485196+HiramSilvey@users.noreply.github.com>
2024-11-24 21:43:54 -0500

    Bind eglot rename fn.

--- .emacs.d/keys.el
+++ .emacs.d/keys.el
@@ -12,7 +12,8 @@
   :bind (("C-c /" . comment-or-uncomment-region)
          ("C-c q" . kill-buffer-and-window)
          ("C-c k c" . kill-current-buffer)
-         ("C-c k o" . clean-buffer-list))
+         ("C-c k o" . clean-buffer-list)
+         ("C-c r" . eglot-rename))
   :init (which-key-add-key-based-replacements "C-c h" "help"))
 
 ;;; keys.el ends here
